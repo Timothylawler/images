285ab899e7aab2f9f7955f91fff3727f33e1e4b1
timothylawler <timothy.karvonen@hotmail.se>
2017-07-16 12:48:48 +0200

    add temp user for get authenticated user

--- testAuth0-backend/routes/user/user.js
+++ testAuth0-backend/routes/user/user.js
@@ -35,6 +35,48 @@ user.post("/", authCheck, (req, res) => {
   //  let userId = req.body.userId;
   //  let sessionAccessToken = req.body.accessToken;
   res.end();
+});
+
+/*  Gets the user with the access token from req.headers['authorization']
+ */
+user.get("/", authCheck, (req, res) => {
+  let authorization = req.headers['authorization'];
+  //console.log(authorization);
+  setTimeout(()=>res.end(JSON.stringify(tempUser)), 1000);
 })
 
 module.exports = user;
+
+
+let tempUser = {
+  userName: "Tika",
+  name: "Timothy Lawler Karvonen",
+  id: "asdasd",
+  bio: "Capturing colurful nature in south Africa. Making sure that every one sees the world as it is.",
+  profileImage: "https://dncache-mauganscorp.netdna-ssl.com/thumbseg/325/325435-bigthumbnail.jpg",
+  content: {
+    count: 4,
+    images: [
+      {
+        id: 1,
+        text: "text",
+        image: "https://dncache-mauganscorp.netdna-ssl.com/thumbseg/325/325435-bigthumbnail.jpg"
+      },
+      {
+        id: 2,
+        text: "text",
+        image: "https://i.ytimg.com/vi/OdIJ2x3nxzQ/hqdefault.jpg"
+      },
+      {
+        id: 3,
+        text: "text",
+        image: "https://us.123rf.com/450wm/ELEN/ELEN1511/ELEN151100020/49021765-winter-nature-scenery-of-woodland-in-sunny-day.jpg?ver=6"
+      },
+      {
+        id: 4,
+        text: "text",
+        image: "https://i.ytimg.com/vi/OdIJ2x3nxzQ/hqdefault.jpg"
+      },
+    ]
+  }
+}
